9fee72469354f404979ab83005510d4a2c8bd62b
MarioChav <mario.chavarria21roque@gmail.com>
2024-12-18 12:44:54 -0600

    Add project files.

---
+++ PA_Proyecto_G3/PA_Proyecto_G3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PA_Proyecto_G3.MvcApplication" Language="C#" %>
---
+++ PA_Proyecto_G3/PA_Proyecto_G3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PA_Proyecto_G3.Startup))]
+namespace PA_Proyecto_G3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
